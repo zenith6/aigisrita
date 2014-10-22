46e21a67f0b3c34fc326828fccefe348f7f6aa2e
Seiji Nitta <zenith6@gmail.com>
2014-10-23 07:51:07 +0900

    パッケージのファイル名にビルド番号を含めるよう変更

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
-'use strict';
-
-var gulp = require('gulp');
+var gulp    = require('gulp');
 var plugins = require('gulp-load-plugins')();
-var info = require('./package.json');
+var shelljs = require('shelljs');
+var info    = require('./package.json');
 
 gulp.task('scripts', function () {
   return gulp.src([
@@ -12,9 +11,17 @@ gulp.task('scripts', function () {
 });
 
 gulp.task('package', function () {
+  var file = info.name + '-' + getVersion() +'.zip';
+
   return gulp.src('src/**/*')
-    .pipe(plugins.zip(info.name + '-' + info.version + '.zip'))
+    .pipe(plugins.zip(file))
     .pipe(gulp.dest('dist'));
 });
+
+function getVersion() {
+  var result = shelljs.exec('git rev-list HEAD --count', {silent: true});
+  var build = result.output.replace(/\n/, '');
+  return info.version + '.' + build;
+}
 
 gulp.task('default', ['scripts', 'package']);
